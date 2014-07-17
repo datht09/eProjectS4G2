e83725681fc34b7bf642edea604844d096393db5
n0gInhMunHg <hungnm_c00749@fpt.aptech.ac.vn>
2014-07-17 09:32:28 +0700

    add function insert query

--- C1208GS4G2eProject/src/java/DataAccess/DBFuction.java
+++ C1208GS4G2eProject/src/java/DataAccess/DBFuction.java
@@ -9,6 +9,8 @@ import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,6 +18,7 @@ import java.sql.SQLException;
  */
 public class DBFuction {
 //----------------get Connection
+
     private static Connection getConnection() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -25,7 +28,7 @@ public class DBFuction {
 
         Connection con = null;
         try {
-            con = (Connection) DriverManager.getConnection("jdbc:mysql://210.245.81.92:3306/FIS", "bkhnjkt","coffee");
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://210.245.81.92:3306/FIS", "bkhnjkt", "coffee");
         } catch (SQLException e) {
             System.out.println("" + e.toString());
         }
@@ -33,8 +36,35 @@ public class DBFuction {
         return con;
 
     }
- //--------------- get PreparedStatement
-    private static PreparedStatement getPreparedStatement(String sql) throws SQLException{
+
+    public boolean insertQuery(int id, String username, String department, String category, String subject, String content, String dateOfLodging, String dateOfClosing, int status) {
+        int result = 0;
+        try {
+
+            Connection conn = getConnection();
+            String sql = "insert into tbl_Query values(?,?,?,?,?,?,?,?,?)";
+            PreparedStatement prst = getPreparedStatement(sql);
+            prst.setInt(1, id);
+            prst.setString(2, username);
+            prst.setString(3, department);
+            prst.setString(4, category);
+            prst.setString(5, subject);
+            prst.setString(6, content);
+            prst.setString(7, dateOfLodging);
+            prst.setString(8, dateOfClosing);
+            prst.setInt(9, status);
+            result = prst.executeUpdate();
+            prst.close();
+            conn.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DBFuction.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result > 0;
+    }
+
+//--------------- get PreparedStatement
+    private static PreparedStatement getPreparedStatement(String sql) throws SQLException {
         return getConnection().clientPrepareStatement(sql);
     }
 }
